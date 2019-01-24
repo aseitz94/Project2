b6ce27316302aef318b22ff4fbc4b1660b608fa7
AshSeitz <ash.seitz94@gmail.com>
2019-01-23 23:12:10 -0500

    git commit - m "redirect link"

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
       password: req.body.password
     })
       .then(function() {
-        res.redirect(307, "/api/login");
+        res.redirect(307, "/login");
       })
       .catch(function(err) {
         console.log(err);
