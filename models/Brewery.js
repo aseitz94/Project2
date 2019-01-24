3ffba84d95108e32a3b877e3e68061c2bcc8bc1b
AshSeitz <ash.seitz94@gmail.com>
2019-01-24 18:14:15 -0500

    git commit -m "prettier fixes"

--- models/Brewery.js
+++ models/Brewery.js
@@ -1,14 +1,13 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var Brewery = sequelize.define("Brewery", {
     breweryName: DataTypes.STRING,
     address: DataTypes.STRING,
     breweryAPIid: DataTypes.STRING
   });
 
-  Brewery.associate = function (models) {
-
+  Brewery.associate = function(models) {
     Brewery.belongsToMany(models.User, {
-      through: 'UserBrewery'
+      through: "UserBrewery"
     });
   };
 
