392dd46e137b8714d03a75d9bf347df033116c25
rhgcodes <rgalleguillos87@outlook.com>
2019-01-27 22:03:23 -0500

    cleanedup

--- routes/BreweryZip.js
+++ routes/BreweryZip.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-/* eslint-disable no-unused-vars */
->>>>>>> 3c9675c50960a349259fcd7e9155d385d9dd460f
 //node packages
 var zipcodes = require("zipcodes");
 var request = require("request");
@@ -18,7 +14,6 @@ var apiKey02 = "dd89d2e84b2df24a8168a303c291c04c";
 var queryURL2 = "&lat=";
 var queryURL3 = "&lng=";
 var queryURL4 = "&radius=";
-<<<<<<< HEAD
 // var testZip = 90210;
 // var testRadius = 50;
 
@@ -31,20 +26,6 @@ var queryURL4 = "&radius=";
 // var breweriesOpenArray = [];
 module.exports = {
   getBreweriesZip: function(zipCode, radius, breweries, certified, callback) {
-=======
-var testZip = 90210;
-var testRadius = 50;
-
-//Array to store all Brewery Objects from API Pull
-//This array is populated using getBreweriesZip function
-var breweriesArray = [];
-
-//Array to store only open Brewey Objects from API Pull
-//this array is populated using the getOpenBreweries function
-var breweriesOpenArray = [];
-module.exports = {
-  getBreweriesZip: function(zipCode, radius, breweries, callback) {
->>>>>>> 3c9675c50960a349259fcd7e9155d385d9dd460f
     var zipObject = zipcodes.lookup(zipCode);
     if (zipObject !== undefined) {
       var latitude = zipObject.latitude;
@@ -59,7 +40,6 @@ module.exports = {
         queryURL4 +
         radius;
       //var queryURL = queryURLNEW + latitude + queryURL3 + longitude + queryURL4 + radius + "&search="+zipCode;
-<<<<<<< HEAD
       console.log("url", queryURL);
       request(queryURL, function(error, response, body) {
         // If the request is successful (i.e. if the response status code is 200)
@@ -86,31 +66,6 @@ module.exports = {
           }
           // result = breweriesArray;
           return callback(breweriesArray, false);
-=======
-      console.log(queryURL);
-      request(queryURL, function(error, response, body) {
-        // If the request is successful (i.e. if the response status code is 200)
-        if (
-          !error &&
-          response.statusCode === 200 &&
-          JSON.parse(body).data !== undefined
-        ) {
-          //empty the Brewery Array
-          breweriesArray = [];
-          for (var i = 0; i < JSON.parse(body).data.length; i++) {
-            breweries[i] = JSON.parse(body).data[i];
-            //add brewery object to external array
-            if (
-              JSON.parse(body).data[i].openToPublic === "Y" &&
-              JSON.parse(body).data[i].isClosed === "N"
-            ) {
-              //validate
-              breweriesArray.push(breweries[i]);
-            }
-          }
-          result = breweriesArray;
-          return callback(result, false);
->>>>>>> 3c9675c50960a349259fcd7e9155d385d9dd460f
         } else {
           return callback(null, true);
         }
@@ -127,11 +82,7 @@ module.exports = {
       if (
         !error &&
         response.statusCode === 200 &&
-<<<<<<< HEAD
         JSON.parse(body).data != undefined
-=======
-        JSON.parse(body).data !== undefined
->>>>>>> 3c9675c50960a349259fcd7e9155d385d9dd460f
       ) {
         result = JSON.parse(body).data;
         return callback(result, false);
@@ -148,11 +99,7 @@ module.exports = {
       if (
         !error &&
         response.statusCode === 200 &&
-<<<<<<< HEAD
         JSON.parse(body).data != undefined
-=======
-        JSON.parse(body).data !== undefined
->>>>>>> 3c9675c50960a349259fcd7e9155d385d9dd460f
       ) {
         result = JSON.parse(body).data;
         return callback(result, false);
@@ -162,30 +109,3 @@ module.exports = {
     });
   }
 };
-<<<<<<< HEAD
-=======
-
-/*
-function getOpenBreweries(allBreweries, openBreweries) {
- //empty the openbreweries array
- openBreweries = [];
- //add only the open breweries from "allBreweries" to the "openBreweries" Array
- for (var i = 0, i < allBreweries.length, i++) {
-   console.log(i);
- }
-} */
-
-/*
-function getOpenCraftBreweries(allBreweries, openBreweries) {
- //empty the openbreweries array
- openBreweries = [];
- //add only the open breweries from "allBreweries" to the "openBreweries" Array
- for (i = 0, i < allBreweries.length, i++) {
-   if (allBreweries[i].openToPublic = "Y" && ) {
-     openBreweries.push(allBreweries[i]);
-   }
- }
-} */
-
-//getBreweriesZip(testZip, testRadius, breweriesArray);
->>>>>>> 3c9675c50960a349259fcd7e9155d385d9dd460f
